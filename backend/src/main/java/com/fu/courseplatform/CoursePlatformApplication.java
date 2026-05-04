92811bd177cc3cf3d65e801b3d9993e8e7d614ae
TuanNPA040905 <tuannpahe190953@gmail.com>
2026-05-04 21:05:53 +0700

    2. Build model

---
+++ backend/src/main/java/com/fu/courseplatform/CoursePlatformApplication.java
@@ -0,0 +1,14 @@
+package com.fu.courseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CoursePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlatformApplication.class, args);
+	}
+
+}
